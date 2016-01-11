a5da300de9394d1ceb3fb28deb45a18fea586ce7
root <xhembedded@163.com>
2016-01-11 22:06:30 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	getPageUrl/__init__.py
    #	getPageUrl/a_href.db
    #	getPageUrl/selectSqlite3.py

--- Proxy/verify_proxy.py
+++ Proxy/verify_proxy.py
@@ -1,23 +1,27 @@
 #! /usr/bin/env python
 # coding=utf-8
+import re, requests, urllib2
 
-import time, requests, urllib2
-
-ip = ''
-port = ''
-s = requests.session()
-proxy = ip + port
-s.proxies = {
-    'http': 'http://' + proxy,
-    'https': 'https://' + proxy
+test_url = {
+    "baidu": 'http://www.baidu.com',
+    "qq": "http://www.qq.com",
+    "sina": "http://sina.com"
 }
-url = 'http://www.qq.com'
-code = s.get(url, timeout=10).status_code  # 0.255999803543
-
-# code = urllib2.urlopen(url).code        #0.0569999217987
-
-if code == 200:
-    print url, 'is OK'
-else:
-    print url, 'code:', code
-print 'using time:', time.time() - stime
+with open("proxy.txt") as f:
+    proxy = f.readlines()
+for p in proxy:
+    proxies = {
+        'http': 'http://' + re.match('\d+\.\d+\.\d+\.\d+:\d+', str(p)).group(0),
+        'https': 'https://' + re.match('\d+\.\d+\.\d+\.\d+:\d+', str(p)).group(0)
+    }
+    # for url in test_url:
+    try:
+        code = requests.get(test_url['baidu'], proxies=proxies, timeout=10).status_code
+        if code == 200:
+            open("ip_data.txt", 'a').write(re.match('\d+\.\d+\.\d+\.\d+:\d+', str(p)).group(0) + '\n')
+            print 'Proxy:', p , test_url['baidu'], 'is OK'
+        else:
+            print 'Proxy:', p , test_url['baidu'], 'code:', code
+    except:
+        print 'Proxy:', p , test_url['baidu'], "connect Error"
+        pass
